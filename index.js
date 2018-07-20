34db34a695f1ae6ef514137d724a2c0c56a29187
Luke Edwards <luke.edwards05@gmail.com>
2018-07-20 01:24:07 -0700

    overwrite index w/ foo

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
-const { format } = require('util');
 const CODES = require('./codes');
+let $ = { enabled:true };
 
-const $ = { enabled:true };
-const RGX = /\x1b\[[0-9]+m/ig;
-
-function print() {
-	let out = format.apply(null, arguments);
-	if (!$.enabled || out.trim().length == 0) return out;
-
-	let i=0, tmp, arr=this.keys, isMulti=!!~out.indexOf('\n');
-	for (; i < arr.length; i++) {
-		tmp = CODES[arr[i]]; // { x1, x2, rgx }
-		out = tmp.beg + out.replace(tmp.rgx, tmp.beg) + tmp.end;
-		isMulti && (out = out.replace(/(\r?\n)/g, `${tmp.end}$1${tmp.beg}`));
-	}
-
-	return out;
+function run(key, str) {
+	let tmp = CODES[key];
+	return tmp.open + str.replace(tmp.rgx, tmp.open) + tmp.close;
 }
 
-function wrap(keys) {
-	let ctx = {};
-	let fn = Object.setPrototypeOf(print.bind(ctx), $);
-	ctx.keys = fn.keys = keys;
+function exec(str) {
+	if (!$.enabled) return str;
+	let arr=this.keys, old=arr[0];
+	while (arr.length > 0) {
+		str = run(arr.shift(), str);
+	}
+	this.keys = [old];
+	return str;
+}
+
+function attach(keys) {
+	let ctx = { keys };
+	let fn = exec.bind(ctx);
+	for (let k in CODES) {
+		Reflect.defineProperty(fn, k, {
+			get() {
+				ctx.keys.push(k);
+				return fn;
+			}
+		});
+	}
 	return fn;
 }
 
 for (let k in CODES) {
+	$[k] = attach([k]);
 	CODES[k] = {
-		beg: `\x1b[${CODES[k][0]}m`,
-		end: `\x1b[${CODES[k][1]}m`,
+		open: `\x1b[${CODES[k][0]}m`,
+		close: `\x1b[${CODES[k][1]}m`,
 		rgx: new RegExp(`\\x1b\\[${CODES[k][1]}m`, 'g')
-	};
-	Object.defineProperty($, k, {
-		get() {
-			return this.keys !== void 0 ? (this.keys.push(k),this) : wrap([k]);
-		}
-	});
-}
-
-$.clear = str => {
-	return str && typeof str === 'string' ? str.replace(RGX, '') : str;
+	}
 }
 
 module.exports = $;
