e7c96db3f233cd6c112510b24ad6ae395a3c071a
Luke Edwards <luke.edwards05@gmail.com>
2018-07-20 22:05:04 -0700

    add integer-type tests

--- test/index.js
+++ test/index.js
@@ -40,6 +40,15 @@ test('nested', t => {
 	t.end();
 });
 
+test('integer', t => {
+	let { red, blue, italic } = CODES;
+	t.is(c.blue(123), ANSI(blue[0]) + '123' + ANSI(blue[1]), '~> basic');
+	t.is(c.red.italic(0), ANSI(italic[0]) + ANSI(red[0]) + '0' + ANSI(red[1]) + ANSI(italic[1]), '~> chain w/ 0');
+	t.is(c.italic(`${c.red(123)} ${c.blue(0)}`), ANSI(italic[0]) + ANSI(red[0]) + '123' + ANSI(red[1]) + ' ' + ANSI(blue[0]) + '0' + ANSI(blue[1]) + ANSI(italic[1]), '~> chain w/ nested & 0');
+	t.is(c.blue(-1), ANSI(blue[0]) + '-1' + ANSI(blue[1]), '~> basic w/ negatives');
+	t.end();
+});
+
 // test('multiline', t => {
 // 	let { blue, bold, red, italic } = CODES;
 // 	t.is(c.blue('hello\nworld'), ANSI(blue[0]) + 'hello' + ANSI(blue[1]) + '\n' + ANSI(blue[0]) + 'world' + ANSI(blue[1]), '~> basic');
