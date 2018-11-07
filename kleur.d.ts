36525ded7707e0217bda19efbd0d984ee28c3ba0
Luke Edwards <luke.edwards05@gmail.com>
2018-11-07 11:41:05 -0800

    refactor: simplify typings

--- kleur.d.ts
+++ kleur.d.ts
@@ -1,56 +1,51 @@
-// TypeScript definitions for kleur
-// Definitions by: Madhav Varshney <https://github.com/madhavarshney>
-// Originally written for ansi-colors
 // Originally by: Rogier Schouten <https://github.com/rogierschouten>
+// Adapted by: Madhav Varshney <https://github.com/madhavarshney>
 
-/**
- * Each method can be called as a function
- * or return a chainable Kleur object
- */
-interface KleurOrFunction extends KleurInstance {
-    (string: string): string;
+// Returns Instance or String (when called with input)
+interface Color {
+	(x: string | number): string;
+	(): Kleur;
 }
 
-interface KleurInstance {
-    /* Colors */
-    black: KleurOrFunction;
-    red: KleurOrFunction;
-    green: KleurOrFunction;
-    yellow: KleurOrFunction;
-    blue: KleurOrFunction;
-    magenta: KleurOrFunction;
-    cyan: KleurOrFunction;
-    white: KleurOrFunction;
-    gray: KleurOrFunction;
+interface Kleur {
+	// Colors
+	black: Color;
+	red: Color;
+	green: Color;
+	yellow: Color;
+	blue: Color;
+	magenta: Color;
+	cyan: Color;
+	white: Color;
+	gray: Color;
 
-    /* Background Colors */
-    bgBlack: KleurOrFunction;
-    bgRed: KleurOrFunction;
-    bgGreen: KleurOrFunction;
-    bgYellow: KleurOrFunction;
-    bgBlue: KleurOrFunction;
-    bgMagenta: KleurOrFunction;
-    bgCyan: KleurOrFunction;
-    bgWhite: KleurOrFunction;
+	// Backgrounds
+	bgBlack: Color;
+	bgRed: Color;
+	bgGreen: Color;
+	bgYellow: Color;
+	bgBlue: Color;
+	bgMagenta: Color;
+	bgCyan: Color;
+	bgWhite: Color;
 
-    /* Modifiers */
-    reset: KleurOrFunction;
-    bold: KleurOrFunction;
-    dim: KleurOrFunction;
-    italic: KleurOrFunction;
-    underline: KleurOrFunction;
-    inverse: KleurOrFunction;
-    hidden: KleurOrFunction;
-    strikethrough: KleurOrFunction;
+	// Modifiers
+	reset: Color;
+	bold: Color;
+	dim: Color;
+	italic: Color;
+	underline: Color;
+	inverse: Color;
+	hidden: Color;
+	strikethrough: Color;
 }
 
-declare let kleur: KleurInstance;
+declare let kleur: Kleur;
 export default kleur;
 
 /**
- * Enable ANSI Colors.
- * Note: You cannot write to ES6 exports and therefore to `kleur.enabled`
- *       in TypeScript with `import * as kleur from 'kleur';`.
- *       Workaround is to use `import kleur = require('kleur');`.
+ * Note: ES6 imports are immutable
+ * Therefore `kleur.enabled` can only be changed via
+ *    import kleur = require('kleur');
  */
 export let enabled: boolean;
