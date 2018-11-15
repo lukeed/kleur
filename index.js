7d8720db15e85219e4c0f5ade103f91cd9360543
Luke Edwards <luke.edwards05@gmail.com>
2018-11-14 16:12:08 -0800

    fix: define color codes inline;
    - no `CODES` shape deopt

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 const { FORCE_COLOR, NODE_DISABLE_COLORS, TERM } = process.env;
 
+function code(open, close) {
+	return {
+		open: `\x1b[${open}m`,
+		close: `\x1b[${close}m`,
+		rgx: new RegExp(`\\x1b\\[${close}m`, 'g')
+	};
+}
+
 const CODES = {
 	// modifiers
-	reset: [0, 0],
-	bold: [1, 22],
-	dim: [2, 22],
-	italic: [3, 23],
-	underline: [4, 24],
-	inverse: [7, 27],
-	hidden: [8, 28],
-	strikethrough: [9, 29],
+	reset: code(0, 0),
+	bold: code(1, 22),
+	dim: code(2, 22),
+	italic: code(3, 23),
+	underline: code(4, 24),
+	inverse: code(7, 27),
+	hidden: code(8, 28),
+	strikethrough: code(9, 29),
 
 	// colors
-	black: [30, 39],
-	red: [31, 39],
-	green: [32, 39],
-	yellow: [33, 39],
-	blue: [34, 39],
-	magenta: [35, 39],
-	cyan: [36, 39],
-	white: [37, 39],
-	gray: [90, 39],
+	black: code(30, 39),
+	red: code(31, 39),
+	green: code(32, 39),
+	yellow: code(33, 39),
+	blue: code(34, 39),
+	magenta: code(35, 39),
+	cyan: code(36, 39),
+	white: code(37, 39),
+	gray: code(90, 39),
 
 	// background colors
-	bgBlack: [40, 49],
-	bgRed: [41, 49],
-	bgGreen: [42, 49],
-	bgYellow: [43, 49],
-	bgBlue: [44, 49],
-	bgMagenta: [45, 49],
-	bgCyan: [46, 49],
-	bgWhite: [47, 49]
+	bgBlack: code(40, 49),
+	bgRed: code(41, 49),
+	bgGreen: code(42, 49),
+	bgYellow: code(43, 49),
+	bgBlue: code(44, 49),
+	bgMagenta: code(45, 49),
+	bgCyan: code(46, 49),
+	bgWhite: code(47, 49)
 };
 
 let $ = {
@@ -61,12 +69,6 @@ function run(arr, str) {
 }
 
 for (let key in CODES) {
-	CODES[key] = {
-		open: `\x1b[${CODES[key][0]}m`,
-		close: `\x1b[${CODES[key][1]}m`,
-		rgx: new RegExp(`\\x1b\\[${CODES[key][1]}m`, 'g')
-	};
-
 	$[key] = txt => {
 		let str = txt == null ? '' : txt+'';
 		return $.enabled && str ? run([key], str) : str || chain(key);
