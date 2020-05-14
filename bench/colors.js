61d0d5852dbe5c4b43f06e4c0f21b5ee64affde5
Luke Edwards <luke.edwards05@gmail.com>
2020-05-14 12:11:08 -0700

    chore: simplify bench runner

--- bench/colors.js
+++ bench/colors.js
@@ -1,60 +1,20 @@
 const { Suite } = require('benchmark');
-const cursor = require('ansi')(process.stdout);
 const color = require('ansi-colors');
 const chalk = require('chalk');
 const kleur = require('..');
 
-const names = [
-  'reset',
-  'bold',
-  'dim',
-  'italic',
-  'underline',
-  'inverse',
-  'hidden',
-  'strikethrough',
-  'black',
-  'red',
-  'green',
-  'yellow',
-  'blue',
-  'magenta',
-  'cyan',
-  'white',
-  'gray',
-  'bgBlack',
-  'bgRed',
-  'bgGreen',
-  'bgYellow',
-  'bgBlue',
-  'bgMagenta',
-  'bgCyan',
-  'bgWhite'
-];
-
-const cycle = (e, nl) => {
-  cursor.eraseLine();
-  cursor.horizontalAbsolute();
-  cursor.write('  ' + e.target);
-  if (nl) cursor.write('\n');
-};
+// All color/method names
+const names = ['reset', 'bold', 'dim', 'italic', 'underline', 'inverse', 'hidden', 'strikethrough', 'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray', 'bgBlack', 'bgRed', 'bgGreen', 'bgYellow', 'bgBlue', 'bgMagenta', 'bgCyan', 'bgWhite'];
 
 function bench(name) {
   console.log(`\n# ${name}`);
   const suite = new Suite();
-  const res = {
-    run: suite.run.bind(suite),
-    add: (key, fn) => {
-      suite.add(key, {
-        onCycle: e => cycle(e),
-        onComplete: e => cycle(e, true),
-        fn: fn
-      });
-      return res;
-    }
-  };
-  return res;
+	const previous = suite.add.bind(suite);
+	suite.on('cycle', e => console.log('  ' + e.target));
+	suite.add = (name, runner) => previous(name.padEnd(16), runner);
+  return suite;
 }
+
 
 bench('All Colors')
   .add('ansi-colors', () => {
@@ -68,6 +28,7 @@ bench('All Colors')
   })
   .run();
 
+
 bench('Stacked colors')
   .add('ansi-colors', () => {
     names.forEach(name => color[name].bold.underline.italic('foo'));
@@ -80,11 +41,13 @@ bench('Stacked colors')
   })
   .run();
 
+
 bench('Nested colors')
   .add('ansi-colors', () => fixture(color))
   .add('chalk', () => fixture(chalk))
   .add('kleur', () => fixture(kleur))
   .run();
+
 
 function fixture(lib) {
   return lib.red(`a red ${lib.white('red')} red ${lib.red('red')} red ${lib.gray('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.blue('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')}red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')}red ${lib.green('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.magenta('red')} red ${lib.red('red')}red ${lib.red('red')} red ${lib.cyan('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.yellow('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} red ${lib.red('red')} message`);
