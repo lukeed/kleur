97b7e3a1203bbbbec2e2d3569221dccd5608413f
Luke Edwards <luke.edwards05@gmail.com>
2020-06-23 14:43:21 -0700

    fix: allow `FORCE_COLOR` override

--- colors.mjs
+++ colors.mjs
@@ -1,7 +1,7 @@
 const { FORCE_COLOR, NODE_DISABLE_COLORS, TERM } = process.env;
 
 export const $ = {
-	enabled: !NODE_DISABLE_COLORS && TERM !== 'dumb' && FORCE_COLOR !== '0' && process.stdout.isTTY
+	enabled: !NODE_DISABLE_COLORS && TERM !== 'dumb' && (!!FORCE_COLOR || process.stdout.isTTY)
 }
 
 function init(x, y) {
--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@
 const { FORCE_COLOR, NODE_DISABLE_COLORS, TERM } = process.env;
 
 const $ = {
-	enabled: !NODE_DISABLE_COLORS && TERM !== 'dumb' && FORCE_COLOR !== '0' && process.stdout.isTTY,
+	enabled: !NODE_DISABLE_COLORS && TERM !== 'dumb' && (!!FORCE_COLOR || process.stdout.isTTY),
 
 	// modifiers
 	reset: init(0, 0),
